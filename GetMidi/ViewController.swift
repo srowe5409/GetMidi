c55c0cc57420dfd9809d88775c59e2cab7cfd5db
srowe5409 <srowe5409@gmail.com>
2017-01-13 13:03:10 -0600

    Initial Commit

---
+++ GetMidi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetMidi
+//
+//  Created by Stephen Rowe on 1/13/17.
+//  Copyright © 2017 Stephen Rowe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
